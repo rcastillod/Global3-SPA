799ca2c31fa1edcab02b7748350e7a80b45e13ca
rcastillod <rodrigo.castillo@global3.cl>
2022-10-05 16:47:13 -0300

    Feat: Noise background

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div></div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -9,5 +10,16 @@
 <style>
 body {
   @apply bg-primary dark:bg-background-color-light;
+}
+body::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-image: url(./assets/images/noise.gif);
+  z-index: -1;
+  opacity: 0.05;
 }
 </style>
