bd76c3e7a87a596986dba97bc03e660f69f44007
rcastillod <rodrigo.castillo@global3.cl>
2023-06-15 17:43:06 -0400

    feat: :sparkles: Load more to projects pagination
    This time the data is from wordpress

--- pages/proyectos.vue
+++ pages/proyectos.vue
@@ -4,12 +4,9 @@ import gql from "graphql-tag";
 
 definePageMeta({ layout: "page" });
 
-const numberOfProjects = ref(20);
-
-const { data, loading, fetchMore } = await useAsyncQuery(
-  gql`
-    query proyectos($first: Int, $last: Int, $after: String, $before: String) {
-      proyectos(first: $first, last: $last, after: $after, before: $before) {
+const query = gql`
+query proyectos($limit: Int) {
+      proyectos(first: $limit) {
         nodes {
           databaseId
           imagenProyecto {
@@ -27,11 +24,13 @@ const { data, loading, fetchMore } = await useAsyncQuery(
         }
       }
     }
-  `,
-  {
-    first: numberOfProjects.value,
-  }
-);
+`
+
+const variables = ref({
+  limit: 20,
+})
+
+const { data, pending, refresh } = await useAsyncQuery(query, variables.value);
 
 const projects = computed(() => {
   if (data && data.value && data.value.proyectos) {
@@ -41,74 +40,13 @@ const projects = computed(() => {
   }
 });
 
-const startCursor = computed(() => {
-  return data.value.proyectos.pageInfo.startCursor;
-});
-const endCursor = computed(() => {
-  return data.value.proyectos.pageInfo.endCursor;
-});
-
-const nextProjects = () => {
-  // Scroll to top after update query
-  window.scrollTo(0, 400);
-
-  fetchMore({
-    variables: {
-      first: numberOfProjects.value,
-      after: endCursor.value,
-    },
-    updateQuery(prev, { fetchMoreResult }) {
-      // Make a copy of existing data
-      const mergedData = {
-        ...prev,
-      };
-
-      // Merge nodes
-      mergedData.proyectos = {
-        ...prev.proyectos,
-        nodes: [...fetchMoreResult.proyectos.nodes],
-      };
-
-      // Update endCursor
-      mergedData.proyectos.pageInfo = fetchMoreResult.proyectos.pageInfo;
-
-      return mergedData;
-    },
-  });
+const loadMoreProjects = () => {
+  if (data.value.proyectos.pageInfo.hasNextPage) {
+    variables.value.limit = variables.value.limit + 20;
+    refresh(variables.value);
+  }
 };
 
-const prevProjects = () => {
-  // Scroll to top after update query
-  window.scrollTo(0, 400);
-
-  fetchMore({
-    variables: {
-      first: null,
-      after: null,
-      last: numberOfProjects.value,
-      before: startCursor.value,
-    },
-    updateQuery(prev, { fetchMoreResult }) {
-      // Make a copy of existing data
-      const mergedData = {
-        ...prev,
-      };
-
-      // Merge nodes
-      mergedData.proyectos = {
-        ...prev.proyectos,
-        nodes: [...fetchMoreResult.proyectos.nodes],
-      };
-
-      // Update endCursor
-      mergedData.proyectos.pageInfo = fetchMoreResult.proyectos.pageInfo;
-
-      return mergedData;
-    },
-  });
-};
-
-// onMounted(() => {});
 </script>
 
 <template>
@@ -117,18 +55,12 @@ const prevProjects = () => {
       <LayoutSiteSection :full="true" subtitle="Descubre nuestros casos" title="Últimos proyectos">
         <div class="proyectos grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 mt-14"
           data-aos="fade-up" data-aos-duration="1000">
-          <LayoutSkeletonProjectCard v-if="loading" v-for="items in 20" />
-          <LayoutProjectCard v-else v-for="(project, index) in projects" :key="project.id" :project="project" />
+          <LayoutProjectCard v-for="(project, index) in projects" :key="project.id" :project="project" />
+          <LayoutSkeletonProjectCard v-if="pending" v-for="items in 20" />
         </div>
         <div class="w-full flex justify-center mt-20 gap-4">
-          <button v-if="data && data.proyectos && data.proyectos.pageInfo.hasPreviousPage" @click="prevProjects"
-            class="inline-block bg-gradient-to-r from-orange-1 via-orange-2 to-orange-3 text-white py-2 px-5 rounded hover:shadow-lg hover:shadow-orange-1/40 transition-all ease-in-out duration-500 link">
-            Anterior
-          </button>
-          <button v-if="data && data.proyectos && data.proyectos.pageInfo.hasNextPage" @click="nextProjects"
-            class="inline-block bg-gradient-to-r from-orange-1 via-orange-2 to-orange-3 text-white py-2 px-5 rounded hover:shadow-lg hover:shadow-orange-1/40 transition-all ease-in-out duration-500 link">
-            Siguiente
-          </button>
+          <ElementsButtonTextAnim v-if="data.proyectos.pageInfo.hasNextPage" @click="loadMoreProjects"
+            text="Cargar Más Cargar Más Cargar Más Cargar Más" :projects="true" />
         </div>
       </LayoutSiteSection>
     </section>
