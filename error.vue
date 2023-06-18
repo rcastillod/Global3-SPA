8be6eedb155fc1e5f8acdb940ee536f11f3201a7
rcastillod <rodrigo.castillo@global3.cl>
2023-06-18 19:29:55 -0400

    fix: :lipstick: Fix the mobile layout of error page

--- error.vue
+++ error.vue
@@ -19,9 +19,9 @@ const handleClearError = () => clearError({ redirect: '/' })
 <template>
   <section class="w-full h-screen">
     <LayoutPageSection class="items-center h-full">
-      <Elements404Svg class="col-span-8" data-aos="zoom-in" data-aos-duration="500" />
-      <div class="col-span-4" data-aos="fade-up" data-aos-duration="1000">
-        <h3 class="text-white dark:text-primary text-4xl font-arimo uppercase mt-8 tracking-wider mb-10">
+      <Elements404Svg class="col-span-12 md:col-span-8" data-aos="zoom-in" data-aos-duration="500" />
+      <div class="col-span-12 md:col-span-4 text-center md:text-left" data-aos="fade-up" data-aos-duration="1000">
+        <h3 class="text-white dark:text-primary text-2xl md:text-4xl font-arimo uppercase tracking-wider mb-10">
           Lo sentimos pero esta pagina no existe.
         </h3>
         <a class="inline-block bg-gradient-to-r from-orange-1 via-orange-2 to-orange-3 text-white py-2 px-5 rounded hover:shadow-lg hover:shadow-orange-1/40 transition-all ease-in-out duration-500 link"
